52b12da9bc967b371306178f1a41d92b2c3641d0
Jonathan Duque <johndduque@gmail.com>
2020-03-31 18:11:41 -0400

    add multithreading, responsive GUI

--- csgocheats/Form1.Designer.cs
+++ csgocheats/Form1.Designer.cs
@@ -32,13 +32,14 @@
             this.ghostHackButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.wallHacksDeactivate = new System.Windows.Forms.Button();
-            this.debugEnableWalls = new System.Windows.Forms.Button();
+            this.cheatStatus = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // ghostHackLabel
             // 
             this.ghostHackLabel.AutoSize = true;
-            this.ghostHackLabel.Location = new System.Drawing.Point(42, 58);
+            this.ghostHackLabel.Location = new System.Drawing.Point(12, 86);
             this.ghostHackLabel.Name = "ghostHackLabel";
             this.ghostHackLabel.Size = new System.Drawing.Size(149, 20);
             this.ghostHackLabel.TabIndex = 0;
@@ -47,7 +48,7 @@
             // 
             // ghostHackButton
             // 
-            this.ghostHackButton.Location = new System.Drawing.Point(197, 50);
+            this.ghostHackButton.Location = new System.Drawing.Point(191, 78);
             this.ghostHackButton.Name = "ghostHackButton";
             this.ghostHackButton.Size = new System.Drawing.Size(86, 37);
             this.ghostHackButton.TabIndex = 1;
@@ -69,7 +70,7 @@
             // wallHacksDeactivate
             // 
             this.wallHacksDeactivate.Enabled = false;
-            this.wallHacksDeactivate.Location = new System.Drawing.Point(289, 50);
+            this.wallHacksDeactivate.Location = new System.Drawing.Point(283, 78);
             this.wallHacksDeactivate.Name = "wallHacksDeactivate";
             this.wallHacksDeactivate.Size = new System.Drawing.Size(130, 37);
             this.wallHacksDeactivate.TabIndex = 3;
@@ -77,22 +78,26 @@
             this.wallHacksDeactivate.UseVisualStyleBackColor = true;
             this.wallHacksDeactivate.Click += new System.EventHandler(this.WallHacksDeactivate_Click);
             // 
-            // debugEnableWalls
+            // cheatStatus
             // 
-            this.debugEnableWalls.Location = new System.Drawing.Point(468, 57);
-            this.debugEnableWalls.Name = "debugEnableWalls";
-            this.debugEnableWalls.Size = new System.Drawing.Size(172, 29);
-            this.debugEnableWalls.TabIndex = 4;
-            this.debugEnableWalls.Text = "Debug Walls";
-            this.debugEnableWalls.UseVisualStyleBackColor = true;
-            this.debugEnableWalls.Click += new System.EventHandler(this.DebugEnableWalls_Click);
+            this.cheatStatus.AutoSize = true;
+            this.cheatStatus.Location = new System.Drawing.Point(22, 49);
+            this.cheatStatus.Name = "cheatStatus";
+            this.cheatStatus.Size = new System.Drawing.Size(139, 20);
+            this.cheatStatus.TabIndex = 4;
+            this.cheatStatus.Text = "Status: Up to date";
+            this.cheatStatus.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
             // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(705, 99);
-            this.Controls.Add(this.debugEnableWalls);
+            this.ClientSize = new System.Drawing.Size(705, 140);
+            this.Controls.Add(this.cheatStatus);
             this.Controls.Add(this.wallHacksDeactivate);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ghostHackButton);
@@ -111,7 +116,8 @@
         private System.Windows.Forms.Button ghostHackButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button wallHacksDeactivate;
-        private System.Windows.Forms.Button debugEnableWalls;
+        private System.Windows.Forms.Label cheatStatus;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- csgocheats/Form1.cs
+++ csgocheats/Form1.cs
@@ -14,16 +14,12 @@ namespace csgocheats
 {
     public partial class main : Form
     {
-        static int client;
-        static string process = "csgo";
-        static int address = 0x0;
-        static VAMemory vam;
-        public Boolean wallHacksEnabled = false;
+        private WallVis walls;
+
+
 
         public main()
         {
-            vam = new VAMemory(process);
-
             InitializeComponent();
         }
 
@@ -39,224 +35,81 @@ namespace csgocheats
 
         private void GhostHackButton_Click(object sender, EventArgs e)
         {
-            wallHacksEnabled = true;
-            if (getModuleAddy()) {
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            if (!backgroundWorker1.IsBusy)
+            {
+                wallHacksDeactivate.Enabled = true;
+                ghostHackButton.Enabled = false;
+                cheatStatus.Text = "Status: wall hacks enabled";
 
-                if (wallHacksEnabled)
-                {
-                    wallHacksDeactivate.Enabled = true;
-                    ghostHackButton.Enabled = false;
-
-                }
-                
+                backgroundWorker1.RunWorkerAsync();
 
 
-                while (wallHacksEnabled) {
-                    
-                    GlowStruct enemy = new GlowStruct() {
-                        r = 1,
-                        g = 0,
-                        b = 0,
-                        a = 1,
-                        rwo = true,
-                        rwuo = true
-                    };
-
-                    GlowStruct team = new GlowStruct()
-                    {
-                        r = 0,
-                        g = 0,
-                        b = 1,
-                        a = 1,
-                        rwo = true,
-                        rwuo = true
-                    };
-
-                    int address;
-                    int i = 1;
-
-                    do
-                    {
-                        address = client + Offsets.oLocalPlayer;
-                        int player = vam.ReadInt32((IntPtr)address);
-
-                        address = player + Offsets.oTeam;
-                        int myTeam = vam.ReadInt32((IntPtr)address);
-
-                        address = client + Offsets.oEntityList + (i - 1) * 0x10;
-                        int entityList = vam.ReadInt32((IntPtr)address);
-
-                        address = entityList + Offsets.oTeam;
-                        int otherTeam = vam.ReadInt32((IntPtr)address);
-
-                        address = entityList + Offsets.oDormant;
-
-                        if (!vam.ReadBoolean((IntPtr)address))
-                        {
-                            address = entityList + Offsets.oGlowIndex;
-                            int glowIndex = vam.ReadInt32((IntPtr)address);
-
-                            if (myTeam == otherTeam)
-                            {
-                                address = client + Offsets.oGlowObject;
-                                int glowObject = vam.ReadInt32((IntPtr)address);
-
-                                int calculation = glowIndex * 0x38 + 0x4;
-                                int current = glowObject + calculation;
-                                vam.WriteFloat((IntPtr)current, team.r);
-
-                                calculation = glowIndex * 0x38 + 0x8;
-                                current = glowObject + calculation;
-                                vam.WriteFloat((IntPtr)current, team.g);
-
-                                calculation = glowIndex * 0x38 + 0xC;
-                                current = glowObject + calculation;
-                                vam.WriteFloat((IntPtr)current, team.b);
-
-                                calculation = glowIndex * 0x38 + 0x10;
-                                current = glowObject + calculation;
-                                vam.WriteFloat((IntPtr)current, team.a);
-
-                                calculation = glowIndex * 0x38 + 0x24;
-                                current = glowObject + calculation;
-                                vam.WriteBoolean((IntPtr)current, team.rwo);
-
-                                calculation = glowIndex * 0x38 + 0x25;
-                                current = glowObject + calculation;
-                                vam.WriteBoolean((IntPtr)current, team.rwuo);
-
-
-
-                            }
-                            else
-                            {
-
-                                address = client + Offsets.oGlowObject;
-                                int glowObject = vam.ReadInt32((IntPtr)address);
-
-                                int calculation = glowIndex * 0x38 + 0x4;
-                                int current = glowObject + calculation;
-                                vam.WriteFloat((IntPtr)current, enemy.r);
-
-                                calculation = glowIndex * 0x38 + 0x8;
-                                current = glowObject + calculation;
-                                vam.WriteFloat((IntPtr)current, enemy.g);
-
-                                calculation = glowIndex * 0x38 + 0xC;
-                                current = glowObject + calculation;
-                                vam.WriteFloat((IntPtr)current, enemy.b);
-
-                                calculation = glowIndex * 0x38 + 0x10;
-                                current = glowObject + calculation;
-                                vam.WriteFloat((IntPtr)current, enemy.a);
-
-                                calculation = glowIndex * 0x38 + 0x24;
-                                current = glowObject + calculation;
-                                vam.WriteBoolean((IntPtr)current, enemy.rwo);
-
-                                calculation = glowIndex * 0x38 + 0x25;
-                                current = glowObject + calculation;
-                                vam.WriteBoolean((IntPtr)current, enemy.rwuo);
-
-                            }
-
-                        }
-                        i++;
-
-
-
-
-                    }
-                    while (i < 65);
-                    Thread.Sleep(10);
-                    //DO SOMETHING.
-
-
-                }
-
-
-
-            }
-            
-
-
-
-
-        }
-
-
-
-        public bool getModuleAddy() {
-            try {
-                Process[] p = Process.GetProcessesByName(process);
-                if (p.Length > 0)
-                {
-                    foreach (ProcessModule m in p[0].Modules)
-                    {
-                        if (m.ModuleName == "client_panorama.dll")
-                        {
-                            client = (int)m.BaseAddress;
-                            return true;
-
-                        }
-                   
-                    }
-                    return true;
-
-
-                }
-                else {
-                    return false;
-                }
-
-
-            }
-            catch (Exception e) {
-                return false;
-
+            } else
+            {
+                MessageBox.Show("Wall hacks already running.");
             }
 
 
-        }
 
-        public struct GlowStruct {
-            public float r;
-            public float g;
-            public float b;
-            public float a;
-            public bool rwo;
-            public bool rwuo;
 
-        }
 
-        public class Offsets {
-            public static int oLocalPlayer = 0xD30B94;  //Likely works
-            public static int oTeam = 0xF4; 
-            public static int oGlowIndex = 0xA428; //likely works
-            public static int oGlowObject = 0x528C8D8; //likely works
-            public static int oEntityList = 0x4D44A24; //likely works
-            public static int oDormant = 0xED; //likely works
         }
 
         private void WallHacksDeactivate_Click(object sender, EventArgs e)
         {
-            wallHacksEnabled = false;
-            wallHacksDeactivate.Enabled = false;
-            ghostHackButton.Enabled = true;
 
-
+            //walls.disableWalling();
+            if (backgroundWorker1.IsBusy)
+            {
+                wallHacksDeactivate.Enabled = false;
+                ghostHackButton.Enabled = true;
+                cheatStatus.Text = "Status: wall hacks disabled";
+                walls.disableWalling();
+                backgroundWorker1.CancelAsync();
+            } else
+            {
+                MessageBox.Show("Walls are not enabled. Nothing to stop.");
+            }
 
         }
+
+
 
         private void Label1_Click_1(object sender, EventArgs e)
         {
 
         }
 
-        private void DebugEnableWalls_Click(object sender, EventArgs e)
+        private void Label2_Click(object sender, EventArgs e)
         {
-            WallVis walls = new WallVis();
-            walls.enableWalling();
+
+        }
+
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                wallHacksDeactivate.Enabled = true;
+                ghostHackButton.Enabled = false;
+                cheatStatus.Text = "Status: wall hacks enabled";
+
+                walls = new WallVis();
+                walls.enableWalling();
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Inside exception");
+                Console.WriteLine(ex.Message);
+                backgroundWorker1.CancelAsync();
+
+               // wallHacksDeactivate.Enabled = false;
+                //ghostHackButton.Enabled = true;
+                //cheatStatus.Text = "Status: " + ex.Message;
+
+            }
+
         }
     }
 }
